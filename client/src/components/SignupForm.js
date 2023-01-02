09b4fd9430c70cde11b647c85adf75b918800813
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2023-01-02 16:02:29 -0700

    still trying to fix the problem

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
@@ -12,7 +12,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [addUser, { error, data }] = useMutation(ADD_USER);
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   useEffect(() => {
     if (error) {
